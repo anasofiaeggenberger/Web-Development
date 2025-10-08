fe4bae610faa642075f553b824e3097092f8630c
anasofiaeggenberger <156016977+anasofiaeggenberger@users.noreply.github.com>
2025-10-07 19:16:47 -0600

    arreglos

--- cv-angular/src/app/app-routing.module.ts
+++ cv-angular/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { EducacionExperienciaComponent } from './components/educacion-experiencia/educacion-experiencia.component';
 import { JobsComponent } from './components/jobs/jobs.component';
 import { StudiesComponent } from './components/studies/studies.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { ProgrammingQuoteComponent } from './components/programming-quote/programming-quote.component';
-import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
-  { 
-    path: 'experience', 
+  {
+    path: 'experience',
     component: EducacionExperienciaComponent,
     children: [
       { path: 'jobs', component: JobsComponent },
